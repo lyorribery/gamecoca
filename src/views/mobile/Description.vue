f7cdc4b27b3f53bc4fd7757e1d5745ca805051c6
unknown <362279150@qq.com>
2023-12-01 13:39:41 +0800

    first commit

---
+++ src/views/mobile/Description.vue
@@ -0,0 +1,57 @@
+<template>
+    <div class="header">
+        <img src="@/assets/images/logo.png" style="width: 40px;height: auto;">
+        <div class="header-btn-box">
+            <i class="iconfont icon-guanbi" style="font-size: 28px;color:#ADB6C4;" @click="closeParse()"></i>
+        </div>
+    </div>
+    <div class="parse-content" v-html="parse"></div>
+</template>
+
+<script setup>
+
+import description from '@/mixin/description'
+
+const {
+    base_img_url,
+    closeParse,
+    parse
+} = description()
+
+</script>
+
+<style lang="scss" scoped>
+.header {
+    width: 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+    z-index: 3;
+    height: calc(50px + env(safe-area-inset-top));
+    background-color: #111923;
+    box-sizing: border-box;
+    padding: 0 10px;
+    padding-top: env(safe-area-inset-top);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-bottom: 1px solid #2C353F;
+    color: #ADB6C4;
+    font-size: 18px;
+    font-weight: bold;
+
+    .header-btn-box {
+        position: fixed;
+        top: calc(env(safe-area-inset-top) + 12px);
+        right: 15px;
+    }
+}
+
+.parse-content {
+    width: 100%;
+    box-sizing: border-box;
+    padding: calc(50px + env(safe-area-inset-top)) 10px env(safe-area-inset-bottom) 10px;
+    overflow-y: auto;
+    color: #ADB6C4 !important;
+}
+</style>
---
+++ src/views/mobile/startGame.vue
@@ -0,0 +1,159 @@
+<template>
+  <div class="start-game">
+    <div class="box" v-if="!game_url">
+      <div class="box-header">
+        <div class="title-row">
+          <span>{{ cur_data.name }}</span>
+          <i
+            class="iconfont"
+            :class="isCollection == 1 ? 'icon-collection' : 'icon-no-collection'"
+            @click="changeCollection"
+          ></i>
+        </div>
+      </div>
+      <img :src="cur_data.mobileIcon" />
+      <div
+        style="
+          width: 100%;
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+        "
+      >
+        <div class="back-btn" @click="back">
+          <i class="iconfont icon-leftarrow" style="font-size: 16px"></i>
+          <span style="padding-left: 10px">Back</span>
+        </div>
+        <div class="play-btn" @click="start">
+          <i v-if="!loading" class="iconfont icon-bofang" style="font-size: 16px"></i>
+          <span v-if="!loading" style="padding-left: 10px">Play</span>
+          <svg
+            v-else
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            width="25px"
+            height="25px"
+            viewBox="0 0 50 50"
+            style="enable-background: new 0 0 50 50"
+            xml:space="preserve"
+          >
+            <path
+              fill="#ADB6C4"
+              d="M25.251,6.461c-10.318,0-18.683,8.365-18.683,18.683h4.068c0-8.071,6.543-14.615,14.615-14.615V6.461z"
+              transform="rotate(275.098 25 25)"
+            >
+              <animateTransform
+                attributeType="xml"
+                attributeName="transform"
+                type="rotate"
+                from="0 25 25"
+                to="360 25 25"
+                dur="0.6s"
+                repeatCount="indefinite"
+              ></animateTransform>
+            </path>
+          </svg>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import startGame from "@/mixin/startGame";
+const {
+  cur_data,
+  loading,
+  game_url,
+  isCollection,
+  changeCollection,
+  back,
+  start,
+} = startGame();
+</script>
+
+<style scoped lang="scss">
+.start-game {
+  width: 100%;
+  box-sizing: border-box;
+  padding-top: env(safe-area-inset-top);
+  padding-bottom: env(safe-area-inset-bottom);
+  overflow-y: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  .back-btn {
+    margin-top: 20px;
+    width: calc((100% - 15px) / 2);
+    height: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #adb6c4;
+    font-size: 15px;
+    border-radius: 6px;
+    background: #2b333b;
+  }
+
+  .play-btn {
+    margin-top: 20px;
+    width: calc((100% - 15px) / 2);
+    height: 40px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #fff;
+    font-size: 15px;
+    border-radius: 6px;
+    background: #005dfe;
+  }
+  .box {
+    width: 100%;
+    box-sizing: border-box;
+    padding: 0 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    img {
+        width: 100%;
+        height: auto;
+        border-radius: 16px;
+        margin-top:20px;
+      }
+
+    .box-header {
+      width: 100%;
+      border-radius: 6px;
+      background-color: #111923;
+      box-sizing: border-box;
+      padding: 20px 10px;
+      display: flex;
+      justify-content: space-between;
+      border-bottom: 2px solid #2d3035;
+
+      .title-row {
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        color: #adb6c4;
+
+        span {
+          font-size: 15px;
+        }
+
+        i {
+          font-size: 25px;
+          color: #005dfe;
+        }
+      }
+
+    
+    }
+  }
+}
+</style>
